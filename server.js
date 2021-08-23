6ea8d4592cd395919aea437f122a4552bb177e48
Mohammad <malnajjar16@gmail.com>
2021-08-23 23:20:45 +0200

    made articles router+adding test articles to index.ejs

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 6890;
+const articlesRouter = require('./routes/articles');
 
 app.set('view engine', 'ejs');
 
+app.use('/articles', articlesRouter);
+
 app.get('/', (req, res) => {
-  res.render('index');
+  const articles = [
+    {
+      title: 'blog 1',
+      createdAt: new Date(),
+      description: 'description 1',
+    },
+    {
+      title: 'blog 2',
+      createdAt: new Date(),
+      description: 'description 2',
+    },
+  ];
+  res.render('index', { articles: articles });
 });
 
 app.listen(PORT, () =>
