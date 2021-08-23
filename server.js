3a3ec0d88f7bf3d8d5209f2cb30eca0fb38f217f
Mohammad <malnajjar16@gmail.com>
2021-08-23 22:43:07 +0200

    basic setup server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 6890;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () =>
+  console.log(`server started at http://localhost:${PORT}`)
+);
