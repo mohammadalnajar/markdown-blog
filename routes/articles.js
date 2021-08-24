863d521090ee4a734a6ed412cd5360164d19d31e
Mohammad <malnajjar16@gmail.com>
2021-08-24 16:09:54 +0200

    show ejs edit feature basics added

--- routes/articles.js
+++ routes/articles.js
@@ -3,8 +3,33 @@ const Article = require('./../models/article');
 const router = express.Router();
 
 router.get('/new', (req, res) => {
-  res.render('articles/new');
+  res.render('articles/new', { article: new Article() });
 });
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const article = await Article.findById(id);
+  console.log(article);
+  res.render('articles/show', { article: article });
+});
+router.get('/edit/:id', async (req, res) => {
+  const { id } = req.params;
+  const article = await Article.findById(id);
+  res.render('articles/edit', { article: article });
+});
+router.post('/', async (req, res) => {
+  const { title, description, markdown } = req.body;
+  let article = new Article({
+    title,
+    description,
+    markdown,
+  });
 
-router.post('/', (req, res) => {});
+  try {
+    article = await article.save();
+    res.redirect(`/articles/${article.id}`);
+  } catch (err) {
+    console.log(err);
+    res.render('articles/new', { article: article });
+  }
+});
 module.exports = router;
